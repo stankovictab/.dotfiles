bbf07db7ce8082e68358a3d3abd9ebafd73869db
djosonges <stankovictab@gmail.com>
2023-03-31 21:50:52 +0200

    Commented out startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,18 +5,18 @@
 # Ideal keyboard polling speed
 # First is the delay, second is repeating speed
 # https://askubuntu.com/a/1014269
-xset r rate 220 35
+# xset r rate 220 35
 
 # Changing mouse speed
 # It goes from -1 to 1
 # KDE has it's own mouse setting, this screws that up
-if [ "$(neofetch wm | tail -c 6)" != 'KWin ' ]
-then
-	notify-send "hi"
-	xinput --set-prop 8 'libinput Accel Speed' -0.6
-fi
+# if [ "$(neofetch wm | tail -c 6)" != 'KWin ' ]
+# then
+#	notify-send "hi"
+# 	xinput --set-prop 8 'libinput Accel Speed' -0.6
+# fi
 
-xbindkeys
+# xbindkeys
 
 # Old mouse scroll on side click
 # xinput set-prop 11 "libinput Scroll Method Enabled" 0, 0, 1
