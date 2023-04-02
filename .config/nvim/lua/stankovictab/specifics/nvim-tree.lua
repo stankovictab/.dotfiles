56bbd941b6c58c9889c0d92a1768c3a305422300
Đorđe Stanković <stankovictab@gmail.com>
2023-04-02 16:55:57 +0200

    nvim-tree fix

--- .config/nvim/lua/stankovictab/specifics/nvim-tree.lua
+++ .config/nvim/lua/stankovictab/specifics/nvim-tree.lua
@@ -5,9 +5,18 @@
 -- The open_on_setup configuration is now depricated, now you need to use this ugly ass function
 -- You can configure it to open only on folders, not on files, on empty buffers, etc, see more here
 -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup
-local function open_nvim_tree()
-  -- open the tree
-  require("nvim-tree.api").tree.open()
+local function open_nvim_tree(data)
+	-- buffer is a directory
+	local directory = vim.fn.isdirectory(data.file) == 1
+
+	if not directory then
+		return
+	end
+
+	-- change to the directory
+	vim.cmd.cd(data.file)
+	-- open the tree
+	require("nvim-tree.api").tree.open()
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
