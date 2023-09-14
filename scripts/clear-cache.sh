06028da874bd496cef47139ef0ff7480f2a9907a
Đorđe Stanković <stankovictab@gmail.com>
2023-09-14 13:21:27 +0200

    Clear Cache Script Improvements

--- scripts/clear-cache.sh
+++ scripts/clear-cache.sh
@@ -5,3 +5,5 @@ sudo rm -rf ~/.cache/spotify/Data
 sudo rm -rf ~/.var/app/com.spotify.Client/cache/spotify/Data
 sudo rm -rf ~/.cache/pip/http
 sudo rm -rf ~/.cache/huggingface
+
+notify-send "Cache cleared!" "Remember to empty the trash! 🗑️"
