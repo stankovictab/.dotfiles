0601840d21353e4e2d77fb794897d79cc69270cd
Đorđe Stanković <stankovictab@gmail.com>
2023-10-22 15:12:05 +0200

    Steam Wallpaper Update

--- .config/nvim/lua/stankovictab/plugins.lua
+++ .config/nvim/lua/stankovictab/plugins.lua
@@ -22,8 +22,8 @@ vim.g.mapleader = '	' -- NOTE - THIS IS TEMPORARY, SEE IF IT WORKS WITHOUT IT HE
 vim.g.mkdp_port = '8885'                                      -- Set port for dark reader to turn off
 vim.g.mkdp_auto_close = 0                                     -- To not close the preview when changing buffers, the preview stays open while the markdown buffer is open
 -- Be careful of this, as you'll get an error if you open another preview at the same port. Maybe figure out a way to get around this with mkdp_auto_close and :MarkdownPreviewToggle
-vim.g.mkdp_markdown_css = '~/Desktop/mkdp_mgz.css'            -- CSS file to style the preview
-vim.g.mkdp_highlight_css = '~/Desktop/mkdp_mgz_highlight.css' -- CSS file to style the preview
+vim.g.mkdp_markdown_css = '~/.config/nvim/lua/stankovictab/specifics/mkdp_mgz.css'            -- CSS file to style the preview
+vim.g.mkdp_highlight_css = '~/.config/nvim/lua/stankovictab/specifics/mkdp_mgz_highlight.css' -- CSS file to style the preview
 
 local plugins = {
 	-- Themes
