c412d38b1e24d7e74d419964416420fdc2367193
Đorđe Stanković <stankovictab@gmail.com>
2023-11-18 02:36:36 +0100

    btop

--- sync.sh
+++ sync.sh
@@ -103,6 +103,10 @@ rm -rf ~/.config/zellij
 ln -sf ~/.dotfiles/.config/zellij ~/.config/zellij
 rm -rf ~/.config/spotify-player
 ln -sf ~/.dotfiles/.config/spotify-player ~/.config/spotify-player
+rm -rf ~/.config/btop
+ln -sf ~/.dotfiles/.config/btop ~/.config/btop
+sudo rm -rf /root/.config/btop
+sudo ln -sf /home/$USER/.dotfiles/.config/btop/ /root/.config/btop
 
 rm -rf ~/scripts
 ln -sf ~/.dotfiles/scripts ~/scripts
