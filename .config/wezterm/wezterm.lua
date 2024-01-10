0d1b154bd1a3cfd4fd17d126e7ca04d8f398d783
Đorđe <stankovictab@gmail.com>
2024-01-10 13:24:12 +0100

    btop background update, wezterm theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,7 +3,8 @@ local config = {}
 
 -- Actual config goes here
 
-config.font = wezterm.font 'JetBrainsMono Nerd Font Mono' -- Ligatures are supported OOTB
-config.color_scheme = 'Wild Cherry (Gogh)' -- 950 themes available OOTB
+config.font = wezterm.font 'JetBrainsMono Nerd Font Mono' -- Ligatures are supported OOTB, unlike Alacritty (~~>)
+config.color_scheme = 'GitHub Dark' -- 950 themes available OOTB
+-- See more here: https://wezfurlong.org/wezterm/colorschemes
 
 return config
