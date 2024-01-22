2a3918cfe73c8a5edf8871a121484135601ce44b
Đorđe <stankovictab@gmail.com>
2024-01-23 00:40:30 +0100

    wezterm config update + vscode shortcuts

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,8 +4,18 @@ local config = {}
 -- Actual config goes here
 
 config.font = wezterm.font 'JetBrainsMono Nerd Font Mono' -- Ligatures are supported OOTB, unlike Alacritty (~~>)
-config.color_scheme = 'GitHub Dark'                       -- 950 themes available OOTB
+config.color_scheme = 'GitHub Dark'                       -- 950 themes available OOTB, nice ones are GitHub Dark, Catppuccin Mocha, Dracula, Batman
 -- See more here: https://wezfurlong.org/wezterm/colorschemes
+
+-- These are colors that override colors from the color scheme selected
+config.colors = {
+	foreground = 'white',
+	-- background = 'black',
+	-- Cursor override works in NeoVim aswell, like it should
+	cursor_bg = 'white',
+	cursor_fg = 'black',
+	-- cursor_border = 'white',
+}
 
 config.hide_tab_bar_if_only_one_tab = true -- Tabs are hidden by default
 
@@ -22,9 +32,6 @@ config.window_frame = {
 	button_bg = '#2b2042',
 	button_hover_fg = '#ffffff',
 	button_hover_bg = '#3b3052',
-}
-
-config.colors = {
 }
 
 config.window_padding = {
