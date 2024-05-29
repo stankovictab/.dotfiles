16954097d78345c510caed45cf57105f51051bda
Đorđe Stanković <stankovictab@gmail.com>
2024-05-29 16:06:33 +0200

    lualine pwd

--- .config/nvim/lua/stankovictab/specifics/lualine.lua
+++ .config/nvim/lua/stankovictab/specifics/lualine.lua
@@ -73,41 +73,47 @@ require('lualine').setup {
 
         -- Shows the current loaded LSP clients - their names and icon
         -- Got this from here https://www.reddit.com/r/neovim/comments/11kumh3/comment/jbacpna/
-        -- Needs two { for some unknown reason, I hate lua
         -- You can click on it to open the LSP info window
-        lualine_x = { {
-            function()
-                local lsps = vim.lsp.get_clients({ bufnr = vim.fn.bufnr() })
-                -- local icon = require("nvim-web-devicons").get_icon_by_filetype(
-                -- 	vim.api.nvim_buf_get_option(0, "filetype")
-                -- )
-                if lsps and #lsps > 0 then
-                    local names = {}
-                    for _, lsp in ipairs(lsps) do
-                        table.insert(names, lsp.name)
+        lualine_x = {
+            {
+                function()
+                    -- Last folder of the current working directory
+                    return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+                end,
+                color = { fg = '#444444', gui='italic,bold' }
+            },
+            {
+                function()
+                    local lsps = vim.lsp.get_clients({ bufnr = vim.fn.bufnr() })
+                    -- local icon = require("nvim-web-devicons").get_icon_by_filetype(
+                    -- 	vim.api.nvim_buf_get_option(0, "filetype")
+                    -- )
+                    if lsps and #lsps > 0 then
+                        local names = {}
+                        for _, lsp in ipairs(lsps) do
+                            table.insert(names, lsp.name)
+                        end
+                        return string.format("%s", table.concat(names, "  ")) -- Line that gets printed
+                    else
+                        -- return icon or ""
+                        return ""
                     end
-                    -- return string.format("[%s %s]", table.concat(names, ", "), icon) -- Original line
-                    return string.format("%s", table.concat(names, "  ")) -- Line that gets printed
-                else
-                    -- return icon or ""
-                    return ""
-                end
-            end,
-            on_click = function()
-                vim.api.nvim_command("LspInfo")
-            end,
-            color = function()
-                -- Set the color to the icon's color
-                -- local _, color = require("nvim-web-devicons").get_icon_cterm_color_by_filetype(
-                -- 	vim.api.nvim_buf_get_option(0, "filetype")
-                -- )
-                -- return { fg = color }
+                end,
+                on_click = function()
+                    vim.api.nvim_command("LspInfo")
+                end,
+                color = function()
+                    -- Set the color to the icon's color
+                    -- local _, color = require("nvim-web-devicons").get_icon_cterm_color_by_filetype(
+                    -- 	vim.api.nvim_buf_get_option(0, "filetype")
+                    -- )
+                    -- return { fg = color }
 
-                -- Set the color to the color of the text in the lualine segment
-                local color = vim.fn.synIDattr(vim.fn.hlID('lualine_c_normal'), 'fg')
-                return { fg = color }
-            end,
-        } },
+                    -- Set the color to the color of the text in the lualine segment
+                    local color = vim.fn.synIDattr(vim.fn.hlID('lualine_c_normal'), 'fg')
+                    return { fg = color }
+                end,
+            } },
 
         -- lualine_y = { 'filetype' },
         -- lualine_y = { require('copilot_status').status_string, },
