7059480d114c432e1bcc9a60c2376674e6d6c9a0
Đorđe Stanković <stankovictab@gmail.com>
2024-06-11 17:20:11 +0200

    gdu into update script

--- scripts/update-manual.sh
+++ scripts/update-manual.sh
@@ -30,3 +30,9 @@ rm lazygit*
 # Installing / updating LazyDocker
 
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+
+# Installing / updating gdu
+
+curl -L https://github.com/dundee/gdu/releases/latest/download/gdu_linux_amd64.tgz | tar xz
+chmod +x gdu_linux_amd64
+sudo mv gdu_linux_amd64 /usr/bin/gdu
