f0b6a2524c1280d28be9c973fd574a4b64325281
Đorđe Stanković <stankovictab@gmail.com>
2024-07-05 15:46:04 +0200

    1Password dotdesktop

--- scripts/dotdesktops.sh
+++ scripts/dotdesktops.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# Dotdesktops
 echo "/usr/share/applications/ <-- Most of the stuff"
-
-echo "$HOME/.local/share/applications <-- Gear Lever, Proton and Wine"
+echo -e "$HOME/.local/share/applications <-- \033[0;34mYour own addons,\033[0m, Gear Lever, Proton and Wine"
 echo "/var/lib/flatpak/exports/share/applications/ <-- For Flatpaks"
+# Icons
+echo "/home/stankovictab/.local/share/icons/ <-- Icons from KDE icon packs" 
+
 echo "You can run the following to update the dotdesktop list of your DE :"
-# Print "hi" in blue
 echo -e "\033[0;34msudo update-desktop-database\033[0m"
 echo "Make sure that the app and icons are available, if something isn't correctly set, the dotdesktop won't update."
 
@@ -15,5 +17,5 @@ read -p "Open dotdesktops in Dolphin? [y/N] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    dolphin /usr/share/applications $HOME/.local/share/applications /var/lib/flatpak/exports/share/applications
+    dolphin /usr/share/applications $HOME/.local/share/applications /var/lib/flatpak/exports/share/applications /home/stankovictab/.local/share/icons/
 fi
